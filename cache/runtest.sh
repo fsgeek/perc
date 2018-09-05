1408650278f78e4f8c5abb8a50125af7bf3c95bd
Tony Mason <fsgeek@gatech.edu>
2018-09-04 21:28:10 -0700

    Updated the shell script to write json data to separate files

--- cache/runtest.sh
+++ cache/runtest.sh
@@ -7,7 +7,8 @@
 #
 timestamp=`date '+%Y_%m_%d__%H_%M_%S'`;
 hostname=`hostname`
-LOG_FILE="./perc-test-"$hostname"-"$timestamp".log"
+BASE_FILE="./perc-test-"$hostname"-"$timestamp""
+LOG_FILE="$BASE_FILE".log""
 date > $LOG_FILE
 echo "git log -1" >> $LOG_FILE
 git log -1 >> $LOG_FILE
@@ -28,21 +29,36 @@ mount >> $LOG_FILE
 if [ -f "/usr/bin/ndctl" ]; then
     echo "ndctl list --namespaces  --human" >> $LOG_FILE 2>&1
     ndctl list --namespaces  --human >> $LOG_FILE 2>&1
+    echo "ndctl list -B -D -R -X" >> $LOG_FILE 2>&1
+    ndctl list -B -D -R -X >> $LOG_FILE 2>&1
+fi
+if [ -f "/usr/bin/ipmctl" ]; then
+       echo "ipmctl show -topology" >> $LOG_FILE 2>&1
+       ipmctl show -topology >> $LOG_FILE 2>&1
 fi
 echo "****** TEST RESULTS *****" >> $LOG_FILE
+
 echo "./perc_progs" >> $LOG_FILE
-./perc_progs >> $LOG_FILE 2>&1
+./perc_progs >> $BASE_FILE"-anonymous.json" 2>&1
+
 echo "./perc_progs -d /tmp/fsgeek-test.dat" >> $LOG_FILE
-./perc_progs -d /tmp/fsgeek-test.dat >> $LOG_FILE 2>&1
-#echo "./perc_progs -d" `pwd`" /fsgeek-test.dat" >> $LOG_FILE
-#.perc_progs -d (`whoami`)/fsgeek-test.dat >> $LOG_FILE 2>&1
-#pwd=`pwd`
+./perc_progs -d /tmp/fsgeek-test.dat >> $BASE_FILE"-tmp.json" 2>&1
+
+#echo "./perc_progs -d" `pwd`"/fsgeek-test.dat" >> $LOG_FILE
+#./perc_progs -d (`whoami`)/fsgeek-test.dat >> $BASE_FILE"-home.json" 2>&1
+
 target=`pwd`/"fsgeek-test.dat"
 echo "./perc_progs -d" $target >> $LOG_FILE
-./perc_progs -d $target >> $LOG_FILE 2>&1
+./perc_progs -d $target >> $BASE_FILE"-cwd.json" 2>&1
+
 if [ -d "/mnt/pmem0p1" ]; then
     echo "./perc_progs -d /mnt/pmem0p1/fsgeek-test.dat" >> $LOG_FILE 
-    ./perc_progs -d /mnt/pmem0p1/fsgeek-test.dat >> $LOG_FILE 2>&1
+    ./perc_progs -d /mnt/pmem0p1/fsgeek-test.dat >> $BASE_FILE"-pmem0p1.json" 2>&1
+fi
+
+if [ -d "/mnt/pmem10" ]; then
+    echo "./perc_progs -d /mnt/pmem10/fsgeek-test.dat" >> $LOG_FILE 
+    ./perc_progs -d /mnt/pmem0p1/fsgeek-test.dat >> $BASE_FILE"-pmem10.json" 2>&1
 fi
 
 
