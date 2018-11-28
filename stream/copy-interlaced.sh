2b42e615948a0ad17b485eaafa5ec26495171e0c
fsgeek <fsgeek@gatech.edu>
2018-11-27 21:05:45 -0500

    Fixed script, updated log file

--- stream/copy-interlaced.sh
+++ stream/copy-interlaced.sh
@@ -12,34 +12,34 @@ echo ./copy_progs -v -l $logfile -p 17
 ./copy_progs -v -l $logfile -p 17
 
 echo "NVM primary, DRAM secondary (pmem3)"
-echo ./copy_progs -v -f /mnt/pmem3/wam-test1.dat -p 17
-./copy_progs -v -f /mnt/pmem3/wam-test1.dat -p 17
+echo ./copy_progs -v -f /mnt/pmem3/wam-test1.dat -p 17 -l $logfile
+./copy_progs -v -f /mnt/pmem3/wam-test1.dat -p 17 -l $logfile
 
 echo "NVM primary, DRAM secondary (pmem0)"
-echo ./copy_progs -v -f /mnt/pmem0/wam-test1.dat -p 17
-./copy_progs -v -f /mnt/pmem0/wam-test1.dat -p 17
+echo ./copy_progs -v -f /mnt/pmem0/wam-test1.dat -p 17 -l $logfile
+./copy_progs -v -f /mnt/pmem0/wam-test1.dat -p 17 -l $logfile
 
 echo "DRAM primary, NVM secondary (pmem3)"
-echo ./copy_progs -v -n /mnt/pmem3/wam-test1.dat -p 17
-./copy_progs -v -n /mnt/pmem3/wam-test1.dat -p 17
+echo ./copy_progs -v -n /mnt/pmem3/wam-test1.dat -p 17 -l $logfile
+./copy_progs -v -n /mnt/pmem3/wam-test1.dat -p 17 -l $logfile
 
 echo "DRAM primary, NVM secondary (pmem0)"
-echo ./copy_progs -v -n /mnt/pmem0/wam-test1.dat -p 17
-./copy_progs -v -n /mnt/pmem0/wam-test1.dat -p 17
+echo ./copy_progs -v -n /mnt/pmem0/wam-test1.dat -p 17 -l $logfile
+./copy_progs -v -n /mnt/pmem0/wam-test1.dat -p 17 -l $logfile
 
 echo "NVM primary, NVM secondary (pmem3)"
-echo ./copy_progs -v -f /mnt/pmem3/wam-test1.dat -n /mnt/pmem3/wam-test2.dat -p 17
-./copy_progs -v -f /mnt/pmem3/wam-test1.dat -n /mnt/pmem3/wam-test2.dat -p 17
+echo ./copy_progs -v -f /mnt/pmem3/wam-test1.dat -n /mnt/pmem3/wam-test2.dat -p 17 -l $logfile
+./copy_progs -v -f /mnt/pmem3/wam-test1.dat -n /mnt/pmem3/wam-test2.dat -p 17 -l $logfile
 
 echo "NVM primary, NVM secondary (pmem3) - sanity check (reversed files)"
-echo ./copy_progs -v -n /mnt/pmem3/wam-test1.dat -f /mnt/pmem3/wam-test2.dat -p 17
-./copy_progs -v -n /mnt/pmem3/wam-test1.dat -f /mnt/pmem3/wam-test2.dat -p 17
+echo ./copy_progs -v -n /mnt/pmem3/wam-test1.dat -f /mnt/pmem3/wam-test2.dat -p 17 -l $logfile
+./copy_progs -v -n /mnt/pmem3/wam-test1.dat -f /mnt/pmem3/wam-test2.dat -p 17 -l $logfile
 
 echo "NVM primary (pmem0), NVM secondary (pmem3)"
-echo ./copy_progs -v -f /mnt/pmem0/wam-test1.dat -n /mnt/pmem3/wam-test2.dat -p 17
-./copy_progs -v -f /mnt/pmem0/wam-test1.dat -n /mnt/pmem3/wam-test2.dat -p 17
+echo ./copy_progs -v -f /mnt/pmem0/wam-test1.dat -n /mnt/pmem3/wam-test2.dat -p 17 -l $logfile
+./copy_progs -v -f /mnt/pmem0/wam-test1.dat -n /mnt/pmem3/wam-test2.dat -p 17 -l $logfile
 
 echo "NVM primary (pmem3), NVM secondary (pmem0) - sanity check (reversed files)"
-echo ./copy_progs -v -n /mnt/pmem0/wam-test1.dat -f /mnt/pmem3/wam-test2.dat -p 17
-./copy_progs -v -n /mnt/pmem0/wam-test1.dat -f /mnt/pmem3/wam-test2.dat -p 17
+echo ./copy_progs -v -n /mnt/pmem0/wam-test1.dat -f /mnt/pmem3/wam-test2.dat -p 17 -l $logfile
+./copy_progs -v -n /mnt/pmem0/wam-test1.dat -f /mnt/pmem3/wam-test2.dat -p 17 -l $logfile
 
