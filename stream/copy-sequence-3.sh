e9960fa17d775806e85f514e5b89c9f65f63d8ce
jarulraj <jarulraj@cs.cmu.edu>
2018-11-28 22:38:33 -0500

    Updated scripts to handle simulated PMEM

--- stream/copy-sequence-3.sh
+++ stream/copy-sequence-3.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 echo "Acquire baseline"
 logfile=`date +copy-%y-%m-%d-%R.log`
-echo "start test" `date` >> $logfile
+echo "start test (v3)" `date` >> $logfile
 echo ./copy_progs -t -l $logfile
-./copy_progs -t -l $logfile
-echo "Single NVM run, multiple DRAM runs"
+./copy_progs -t -p 1 -l $logfile
+echo "Single emulated NVM run, multiple DRAM runs"
 for i in {1..20}
 do
    echo ./copy_progs -n /mnt/pmem0/copy-test -p 1 -s 23 -a $i -l $logfile
    ./copy_progs -n /mnt/pmem0/copy-test -p 1 -s 23 -a $i -l $logfile
 done
-echo "Single DRAM run, multiple NVM runs"
+echo "Single DRAM run, multiple emulated NVM runs"
 for i in {1..20}
 do
    echo ./copy_progs -f /mnt/pmem0/copy-test -p 1 -s 23 -a $i -l $logfile
@@ -19,13 +19,10 @@ done
 echo "Sanity: run DRAM in separate nodes"
 echo ./copy_progs -p 1 -s 25 -l $logfile
 ./copy_progs -p 1 -s 25 -l $logfile
-echo "Sanity: run DRAM in same node, single NVM in separate node"
+echo "Sanity: run DRAM in same node, single emulated NVM in separate node"
 for i in {1..20}
 do
-   echo ./copy_progs -n /mnt/pmem0/copy-test -p 25 -s 1 -a $i -l $logfile
-   ./copy_progs -n /mnt/pmem0/copy-test -p 25 -s 1 -a $i -l $logfile
+   echo ./copy_progs -n /mnt/pmem0/copy-test -p 1 -s 25 -a $i -l $logfile
+   ./copy_progs -n /mnt/pmem0/copy-test -p 1 -s 25 -a $i -l $logfile
 done
-echo "Sanity: single NVM one node, single NVM second node"
-echo ./copy_progs -f /mnt/pmem0/copy-test -p 1 -n /mnt/pmem6/copy-test -s 25 -l $logfile
-./copy_progs -f /mnt/pmem0/copy-test -p 1 -n /mnt/pmem6/copy-test -s 25 -l $logfile
 echo "end test" `date` >> $logfile
